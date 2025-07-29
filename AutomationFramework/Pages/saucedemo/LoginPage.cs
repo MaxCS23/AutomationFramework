bf36afcce670ca9fc0c529a3e99f8174779931e2
Max Cortes <maxcortes23@hotmail.com>
2025-07-29 11:43:30 -0600

    Add initial code to saucedemo LoginPage

--- AutomationFramework/Pages/saucedemo/LoginPage.cs
+++ AutomationFramework/Pages/saucedemo/LoginPage.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using AutomationFramework.Core;
+using OpenQA.Selenium;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,19 @@ using System.Threading.Tasks;
 
 namespace AutomationFramework.Pages.saucedemo
 {
-    internal class LoginPage
+    internal class LoginPage : BasePage
     {
+        private readonly By usernameField = By.Id("user-name");
+        private readonly By passwordField = By.Id("password");
+        private readonly By loginButton = By.Id("login-button");
+
+        public LoginPage(IWebDriver driver) : base(driver) {}
+
+        public void Login(string username, string password) 
+        {
+            SendKeys(usernameField, username);
+            SendKeys(passwordField, password);
+            Click(loginButton);        
+        }
     }
 }
